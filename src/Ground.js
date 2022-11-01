bbd79874c7af9c042d8b03b4525fdcf8c8e8093c
Joseph Damiba <joe@fluxon.com>
2022-11-01 01:21:16 -0400

    first commit

---
+++ src/Ground.js
@@ -0,0 +1,47 @@
+import { MeshReflectorMaterial } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import { useEffect } from "react";
+import { LinearEncoding, RepeatWrapping, TextureLoader } from "three";
+
+export function Ground() {
+  const [roughness, normal] = useLoader(TextureLoader, [
+    process.env.PUBLIC_URL + "textures/terrain-roughness.jpeg",
+    process.env.PUBLIC_URL + "textures/terrain-normal.jpeg",
+  ]);
+
+  useEffect(() => {
+    [normal, roughness].forEach((texture) => {
+      texture.wrapS = RepeatWrapping;
+      texture.wrapT = RepeatWrapping;
+      texture.repeat.set(5, 5);
+    });
+    normal.enconding = LinearEncoding;
+  }, [normal, roughness]);
+
+  return (
+    <mesh rotation-x={-Math.PI * 0.5} castShadow recieveShadow>
+      <planeGeometry args={[30, 30]} />
+      <MeshReflectorMaterial
+        normalMap={normal}
+        normalScale={[0.15, 0.15]}
+        roughnessMap={roughness}
+        envMapIntensity={0}
+        dithering={true}
+        color={[0.015, 0.015, 0.015]}
+        roughness={0.7}
+        blur={[1000, 400]}
+        mixBlur={30}
+        mixStrength={80}
+        mixContrast={1}
+        resolution={1024}
+        mirror={0}
+        depthScale={0.01}
+        minDepthThreshold={0.9}
+        maxDepthThreshold={1}
+        depthToBlurRatioBias={0.25}
+        debug={0}
+        reflectorOffset={0.2}
+      />
+    </mesh>
+  );
+}
